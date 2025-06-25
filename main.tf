bbadb0f96e2c77781966caef5b1d7a85b5525259
adilnawaz54 <adilnawaz54@gmail.com>
2025-06-25 12:52:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "s3" {
+    bucket         = "tfstate-bucket-54"
+    key            = "terraform-drift-demo/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "demo" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  root_block_device {
+    volume_size           = var.volume_size   # 20 GB root volume
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "Terraform-Drift-Demo"
+  }
+}
